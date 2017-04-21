9cc12204193bb8bcb599d32c282a4cb5d4116469
Andrew Hudson <ahudson@gmail.com>
2017-04-21 15:33:10 +0100

    update Navigation to use new idb method

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,35 +13,28 @@ class Navigation extends Component {
 	}
 
 	componentDidMount() {
-		fetch('https://big-andy.co.uk/wp-json/wp/v2/pages')
-			.then(function(response) {
-				console.log('doing a page fetch');
-				// Convert to JSON
-				return response.json();
-			}).then((pages) => {
-				pagesStore.outbox('readwrite')
-					.then(db => db.getAll())
-					.then(allObjs => {
-						return new Promise((resolve, reject) => {
-							if (allObjs.length >= 1) {
-								console.log('already have pages in indexedDB')
+		pagesStore.outbox('readwrite')
+			.then(db => db.getAll())
+			.then(allObjs => {
+				return new Promise((resolve, reject) => {
+					if (allObjs.length >= 1) {
+						console.log('already have pages in indexedDB')
 
-								resolve(showPages(allObjs));
-							} else {
-								console.log('do not have posts');
+						resolve(showPages(allObjs));
+					} else {
+						console.log('do not have posts');
 
-								fetchFromAPI('pages', 10).then((pages) => {
-									resolve(showPages(pages, true));
-								});
-							}
-						})
-					}).then(pages => {
-						console.log('Here are the pages', pages);
-
-						this.setState({
-							pages
+						fetchFromAPI('pages', 10).then((pages) => {
+							resolve(showPages(pages, true));
 						});
-					});
+					}
+				})
+			}).then(pages => {
+				console.log('Here are the pages', pages);
+
+				this.setState({
+					pages
+				});
 			});
 	}
 
