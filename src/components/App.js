2d3fc0b79235ee36d8d9192b030608cc21c61c6d
Andrew Hudson <ahudson@gmail.com>
2017-04-25 19:38:07 +0100

    combine showPages and showPosts back into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import Navigation from './Navigation';
 import Posts from './Posts';
@@ -8,8 +8,6 @@ import Article from './Article';
 import SinglePage from './SinglePage';
 import Notes from './Notes';
 
-import showPosts from './helpers/showPosts';
-import showPages from './helpers/showPages';
 import store from './helpers/store';
 import fetchFromAPI from './helpers/fetchFromAPI';
 
@@ -21,7 +19,59 @@ class App extends Component {
 			posts: [],
 			pages: [],
 		};
-	}
+	};
+
+	showPosts = (posts, deep = false) => {
+		return posts.map((item, i) => {
+			if (deep === false) {
+				item = item.post;
+			}
+
+			return (
+				<Article
+					content={ item.content }
+					excerpt={ item.excerpt }
+					link={ `/posts/${item.id}` }
+					title={ item.title }
+					isList={ true }
+					key={ i }
+				/>
+			);
+		});
+	};
+
+	showPages = (pages, deep = false) => {
+		return pages.map(item => {
+			if (deep === false) {
+				item = item.post;
+			}
+
+			if (item.slug === 'style-guide') {
+				return null;
+			}
+
+			let href = `/pages/${item.slug}`;
+
+			if (item.slug === 'home') {
+				href = '/';
+			}
+			return (
+				<Link
+					to={{
+					  pathname: href,
+					  state: {
+						  blogInfo: {
+							  'content': item.content,
+				  		  }
+			  		}
+					}}
+					key={ item.id }
+				>
+					{ item.title }
+				</Link>
+			);
+		});
+	};
 
 	componentDidMount() {
 		store.outbox('posts', 'readwrite')
@@ -31,12 +81,12 @@ class App extends Component {
 					if (allObjs.length >= 1) {
 						// console.log('already have posts in indexedDB')
 
-						resolve(showPosts(allObjs));
+						resolve(this.showPosts(allObjs));
 					} else {
 						// console.log('do not have posts');
-
+						// Do not have posts so fetch some from API
 						fetchFromAPI('posts', this.postsNumber).then((posts) => {
-							resolve(showPosts(posts, true));
+							resolve(this.showPosts(posts, true));
 						});
 					}
 				})
@@ -45,6 +95,7 @@ class App extends Component {
 					posts
 				});
 			});
+
 		store.outbox('pages', 'readwrite')
 			.then(db => db.getAll())
 			.then(allObjs => {
@@ -52,18 +103,18 @@ class App extends Component {
 					if (allObjs.length >= 1) {
 						// console.log('already have pages in indexedDB')
 
-						resolve(showPages(allObjs));
+						resolve(this.showPages(allObjs));
 					} else {
 						// console.log('do not have posts');
 
 						fetchFromAPI('pages', 10).then((pages) => {
-							resolve(showPages(pages, true));
+							resolve(this.showPages(pages, true));
 						});
 					}
 				})
 			}).then(pages => {
 				// console.log('Here are the pages', pages);
-
+				// Populate this.state.pages with the returned pages
 				this.setState({
 					pages
 				});
@@ -78,8 +129,6 @@ class App extends Component {
 						(this.state.pages.length > 0) &&
 							<Navigation pages={ this.state.pages }/>
 					}
-
-
 					<main>
 						<div className="container container--main">
 							<section className="main__posts">
